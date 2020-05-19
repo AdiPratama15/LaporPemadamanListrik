6e963007bd702d17b09fc345265daa7263629dc2
AdiPratama15 <adipratama@protonmail.com>
2020-05-19 22:24:52 +0700

    penambahan default konfigurasi koneksi.php untuk OTP SMS feature API

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+// konfigurasi koneksi
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "members";
+$koneksi = mysqli_connect($host,$user,$password,$database);
+?>
