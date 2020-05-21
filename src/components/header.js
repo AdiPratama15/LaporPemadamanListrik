6d7111322ca3b829abe2d2fb19453f0dfc094d55
AdiPratama15 <adipratama@protonmail.com>
2020-05-22 01:29:43 +0700

    Add pondasi component header.js

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        <div className='header-logo'>
+          {/*<img src='/logo.svg' />*/}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
