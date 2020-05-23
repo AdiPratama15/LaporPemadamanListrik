abc7f19aec6d05ceb98be5e582d7e903d9d4a1a6
advset <fabiola1241996@gmail.com>
2020-05-23 20:28:44 +0700

    Whatsapp Verification Possibility
    It is possible to send the OTP via WhatsApp instead of SMS (just like T*k*pedia does)
    I haven't integrated it, just inserting codes as comment

--- form-login.php
+++ form-login.php
@@ -24,6 +24,20 @@ if (isset($_POST['submit'])) {
         // kirim sms
         $message = "KODE OTP ANDA ADALAH : $new_otp";
         $send = sendsms($member['no_hp'], $message);
+        // kalau ingin WA
+        /*
+        $my_apikey = "API Key Anda"; 
+        $destination = "No Tujuan"; 
+        $message = "KODE OTP ANDA ADALAH : $new_otp"; 
+        $api_url = "http://panel.apiwha.com/send_message.php"; 
+        $api_url .= "?apikey=". urlencode ($my_apikey); 
+        $api_url .= "&number=". urlencode ($destination); 
+        $api_url .= "&text=". urlencode ($message); 
+        $my_result_object = json_decode(file_get_contents($api_url, false)); 
+        echo "<br>Result: ". $my_result_object->success; 
+        echo "<br>Description: ". $my_result_object->description;
+        echo "<br>Code: ". $my_result_object->result_code; 
+        */
         // aktifkan session
         session_start();
         $_SESSION['member_id']=$member['member_id'];
